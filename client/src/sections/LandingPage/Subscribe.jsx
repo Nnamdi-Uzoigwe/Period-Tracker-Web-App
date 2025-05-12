5e4b5c6d8223c321a15459c081a0be082cdd76c1
nnamdi <uzonnamdi31@gmail.com>
2025-05-12 21:39:29 +0100

    changed

--- client/src/sections/LandingPage/Subscribe.jsx
+++ client/src/sections/LandingPage/Subscribe.jsx
@@ -4,7 +4,7 @@ export default function Subscribe() {
     return (
         <div className="px-8 lg:px-40 py-20 bg-gray-200">
             <div className="flex flex-col lg:flex-row text-gray-700 justify-between items-center">
-                <div className="text-center lg:text-left">
+                <div className="text-left mb-4 lg:mb-0">
                     <h4 className="text-xl font-semibold mb-2">Stay Updated</h4>
                     <p className="mb-2">
                         Get the latest tips and insights on cycle tracking. No spam, ever.
@@ -17,7 +17,7 @@ export default function Subscribe() {
                         placeholder="Enter your email..."
                         className="border-2 border-gray-500 p-3 w-[350px] rounded-lg"     
                     />
-                    <Button>Submit</Button>
+                    <button className="cursor-pointer bg-purple-500 hover:bg-purple-600 text-white py-3 px-8 rounded-md w-full lg:w-fit">Submit</button>
                 </form>
             </div>
         </div>
